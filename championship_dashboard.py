f03aabdb523a17c186c7c56878700c3128e32306
Becky O'Connor <becky.oconnor@snowflake.com>
2025-10-07 10:10:42 +0100

    erer

--- championship_dashboard.py
+++ championship_dashboard.py
@@ -394,7 +394,8 @@ def main():
     
     # Filters in main page using form
     # Use stylesheet-driven headings
-    st.markdown('<h3 class="wsc-h3">Select Age and Gender then Retrieve Data</h3>', unsafe_allow_html=True)
+    st.markdown('<h3 class="wsc-h3">Championship Scoreboard</h3>', unsafe_allow_html=True)
+    st.markdown('Select Age and Gender then Retrieve Data', unsafe_allow_html=True)
     
     with st.container():
         col1, col2, col3 = st.columns([2, 2, 1])
